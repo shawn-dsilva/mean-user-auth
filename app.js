60119bfaa1a25482f34606db85922ec0b6ecec46
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2018-11-06 17:01:40 +0530

    another commit

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+consts= config = require('./config/database');
+
 
 const app = express();
 const port = 3000;
 const users = require('./routes/users');
+
+
+//MongoDB
+mongoose.connect(config.database);
+//On connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+});
+//If error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error  ' + config.database);
+});
 
 //CORS
 app.use(cors());
@@ -26,6 +40,7 @@ app.listen(port, () => {
 });
 
 //ROUTES
+
 //Index Route
 app.get('/', (req,res)=> {
     res.send('Invalid Endpoint');
