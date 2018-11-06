0f1fa93113aad5142e4bc24296d33848a041c914
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2018-11-06 19:29:13 +0530

    plain object error fixed

--- routes/users.js
+++ routes/users.js
@@ -40,7 +40,7 @@ router.post('/authenticate', (req, res, next) => {
         User.comparePassword(password, user.password, (err, isMatch) => {
             if(err) throw err;
             if(isMatch){
-                const token = jwt.sign(user, config.secret, {
+                const token = jwt.sign(user.toJSON(), config.secret, { //USER OBJECT TURNED TO JSON
                     expiresIn: 604800 //a week
                 });
 
